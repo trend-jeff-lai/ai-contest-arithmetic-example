bac32228f171aa5343e2ef2bad9389295d5a7c30
Jeff Lai <jeff_lai@trendmicro.com>
2023-08-01 16:58:51 +0800

    Add display menu

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+
+def display_menu():
+    print("Select operation.")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Square root")
+    print("6. Square")
+    print("7. Cube")
+    print("0. Exit")
+
+
+if __name__ == "__main__":
+    display_menu()
+
